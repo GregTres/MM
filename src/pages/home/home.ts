a0a8310c4a24ec4069fa657d2be7c874cde8a7a1
gregory <gtrestour@gmail.com>
2018-03-28 15:43:24 +0200

    renommage result

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,17 +3,16 @@ import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
 
 export interface resultat {
-  title: string;
-  author: string;
-  date: number;
-  image: string;
-
+  original_title: string;
+  release_date: string;
+  poster_path: string;
+  vote_average : number;
 }
 
 const fauxresultats: resultat[] = [
-  { title: 'Star Wars 1', author: 'George lucas', date: 1950, image: 'https://www.bedetheque.com/media/Couvertures/StarWarsEpisode1_23052002.jpg' },
-  { title: 'Star Wars 2', author: 'George lucas', date: 1960, image: 'http://www.filmspourenfants.net/video/images/starwars2.jpg' },
-  { title: 'Star Wars 3', author: 'George lucas', date: 1970, image: 'https://vignette.wikia.nocookie.net/thegamingfamily/images/1/1f/Star-Wars-3-Revenge-of-the-Sith-%E0%B8%8B%E0%B8%B4%E0%B8%98%E0%B8%8A%E0%B8%B3%E0%B8%A3%E0%B8%B0%E0%B9%81%E0%B8%84%E0%B9%89%E0%B8%99.jpg/revision/latest?cb=20140922193631' }
+  { original_title: 'Star Wars 1', vote_average: 5.8, release_date: "1950", poster_path: 'https://www.bedetheque.com/media/Couvertures/StarWarsEpisode1_23052002.jpg' },
+  { original_title: 'Star Wars 2', vote_average: 5.8, release_date: "1960", poster_path: 'http://www.filmspourenfants.net/video/images/starwars2.jpg' },
+  { original_title: 'Star Wars 3', vote_average: 5.8, release_date: "1970", poster_path: 'https://vignette.wikia.nocookie.net/thegamingfamily/images/1/1f/Star-Wars-3-Revenge-of-the-Sith-%E0%B8%8B%E0%B8%B4%E0%B8%98%E0%B8%8A%E0%B8%B3%E0%B8%A3%E0%B8%B0%E0%B9%81%E0%B8%84%E0%B9%89%E0%B8%99.jpg/revision/latest?cb=20140922193631' }
 ]
 
 @Component({
